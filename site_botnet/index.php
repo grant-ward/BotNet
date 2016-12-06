8eea49cfdf73789530de9b3ab8e9b74f6cd8e3de
grant-ward <grant_ward@mail2tor.com>
2016-12-05 22:39:28 -0200

    Create index.php

---
+++ site_botnet/index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<title>BOTNET 1.0</title>
+	 <meta charset="utf-8">
+	  <link rel="stylesheet" type="text/css" href="css/estilo.css">
+	    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+
+  <ul>
+  <li><a href="#">INICIO</a></li>
+  <li><a href="#">SHELL</a></li>
+  <li><a href="#">DDOS</a></li>
+  <li><a href="#">SCREENSHOT</a></li>
+
+</ul>
+
+<div class="main">
+  
+    <img src="../img/skull.jpg">
+
+
+  
+</div>
+
+</body>
+</html>
