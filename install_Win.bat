9be558d83ffaac809f99e7c7233e6882e08ab3b3
grant-ward <grant_ward@mail2tor.com>
2017-01-26 00:27:37 +0000

    Add files via upload

---
+++ install_Win.bat
@@ -0,0 +1,11 @@
+echo off
+cls
+echo "This script will be use to install all the dependencies for use the botnet in windows!"
+pause
+echo "installing dependencies with pip!"
+py -m pip install requests
+py -m pip install cherrypy
+py -m pip install winreglib
+py -m pip install pyinstaller
+py -m pip install pillow
+py -m pip install pypiwin32
